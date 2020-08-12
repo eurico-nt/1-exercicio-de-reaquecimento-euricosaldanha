b7de21c661e2240b0922ebb6188f5206fa426c76
Michael da Costa Móra <michael.c.mora@gmail.com>
2020-08-12 18:14:00 -0300

    Initial commit

---
+++ RaqueciApp/src/test/java/br/pucrs/alav/RecursionTests.java
@@ -0,0 +1,16 @@
+package br.pucrs.alav;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class RecursionTests {
+
+	@Test
+	public void test() { 
+		assertTrue("Será?", true);
+	}
+
+}
